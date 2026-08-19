a07febdf13b5d00548fa5ed1540dba38cbb97ccd
Lim Kai Ying <30360639+LKY1999@users.noreply.github.com>
2026-08-19 11:56:12 +0800

    Fix KDF salt mismatch: use hex string (matching JS enc.encode)

--- assets/auth-config.js
+++ assets/auth-config.js
@@ -1,1 +1,1 @@
-var AUTH_CONFIG = {"pw_salt": "79b5e892f82f61bf64be5aa3cd3b196a", "pw_hash": "8292738b63d2bf458d0d069cd63d1934bd4d1879bbec2038a01b3e1c416d26a5", "kdf_salt": "3aab1439ae4b2863fb51168470ffab00"};
+var AUTH_CONFIG = {"pw_salt": "fb595a3bacac62ce62126bb4abc1546b", "pw_hash": "2441103a08d79ee153263867e36edb82953741cda8b846ea8f818fb89c4fba00", "kdf_salt": "01438f4f33f484c7d73d1a4a53670b6c"};
