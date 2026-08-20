a18e41e73e546203836b0331a016c5e878c41254
Lim Kai Ying <30360639+LKY1999@users.noreply.github.com>
2026-08-20 09:06:54 +0800

    Reencrypt latest content

--- assets/auth-config.js
+++ assets/auth-config.js
@@ -1,1 +1,1 @@
-var AUTH_CONFIG = {"pw_salt": "960ec5d07d18c2444c1d6904537f71c7", "pw_hash": "91e58142e65c7a91259af98d3f0476418f3204939ff63404eb2ab4de0305d88c", "kdf_salt": "1375bc058899eb78b1af012267006ce6"};
+var AUTH_CONFIG = {"pw_salt": "dbc63a9d763d54d8ba301a2ebd867dc8", "pw_hash": "ee48cb7883eb0629b73c31f8ebe6f8aff8d9e46589bdea37d673edc50e0e4a0e", "kdf_salt": "f3b745d4ac8e9de1f7a02cdc1bef96de"};
